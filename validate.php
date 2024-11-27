dbffcfe9a660a36d9504fdce23737b304a4b7963
aayush123aa <140230197+aayush123aa@users.noreply.github.com>
2024-11-27 14:50:46 +0545

    Add files via upload

---
+++ validate.php
@@ -0,0 +1,28 @@
+<?php
+
+$fname= $_POST["aayush_fname"];
+$lname= $_POST["aayush_lname"];
+$email= $_POST["aayush_email"];
+$age= $_POST["aayush_age"];
+$gender= $_POST["aayush_gen"];
+$faculty=$_POST["aayush_faculty"];
+$num=$_POST["aayush_con_num"];
+
+$fname_pattern="//"
+$lname_pattern=
+$email_pattern=
+$age_pattern=
+$gender_pattern=
+$faculty_pattern=
+$number_pattern=
+
+if(empty($fname) || empty($lname) || empty($age) || empty($gender) || empty($faculty) || empty($email) || empty($num) || )
+{
+    echo "Please fill all the fields";
+}
+else
+{
+    
+}
+
+?>
